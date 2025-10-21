d14fcc65e0c9a0d67d2f5bdd734f29c2ea7be8eb
hujpocinguha <2301221@s.asojuku.ac.jp>
2025-10-21 11:33:29 +0900

    Add files via upload

---
+++ U16.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <h1>献立家</h1>
+<a href="U17.php">ユーザ退会処理 ></a>
+
+</body>
+</html>
---
+++ U19.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <h1>献立家</h1>
+    <form action="U20" method="post">
+        <label for="">
+        管理者名
+        <p><input type="text" name="ID" id=""></p>
+        </label>
+        <label for="">
+        パスワード（半角英数8文字以上）
+       <p><input type="password" name="passward" id=""></p> 
+        </label>
+        <label for="">
+        メールアドレス
+       <p><input type="email" name="email" id=""></p> 
+        </label>
+        <button type="submit">登録</button>
+</form>
+</body>
+</html>
