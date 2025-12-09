48dce6950794916f60cc55ec7f7bafdb9f09096d
hujpocinguha <2301221@s.asojuku.ac.jp>
2025-12-09 10:34:14 +0900

    Update U21Confirm.php

--- U21Confirm.php
+++ U21Confirm.php
@@ -40,7 +40,7 @@ try {
                     header('Location: ./U20.php');
                     exit();
             }
-         
+         $id=0;
 
 
  
@@ -89,7 +89,7 @@ body {
 
 <!-- 戻るボタン -->
 <form action="U20.php" method="post">
-    <input type="hidden" name="id" value="0">
+    <input type="hidden" name="id" value="<?= htmlspecialchars($id) ?>">
     <input type="hidden" name="name" value="<?= htmlspecialchars($name, ENT_QUOTES); ?>">
     <input type="hidden" name="email" value="<?= htmlspecialchars($mail, ENT_QUOTES); ?>">
     <input type="hidden" name="password" value="<?= htmlspecialchars($password, ENT_QUOTES); ?>">
