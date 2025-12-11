3e95781d78c9113fa8d021fd1f22e753742b08be
hujpocinguha <2301221@s.asojuku.ac.jp>
2025-12-11 10:05:16 +0900

    修正

--- U22Done.php
+++ U22Done.php
@@ -120,7 +120,7 @@ KondateHause 管理システム";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>管理者登録完了</title>
         <style>
     body {
     margin: 0;
