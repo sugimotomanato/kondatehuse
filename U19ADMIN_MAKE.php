101dea77b33e34e63883e588a763b9096460bf30
hujpocinguha <2301221@s.asojuku.ac.jp>
2025-12-15 12:28:00 +0900

    タイトル名追加

--- U19ADMIN_MAKE.php
+++ U19ADMIN_MAKE.php
@@ -8,7 +8,7 @@ unset($_SESSION['error']); // 1回表示したら消す
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>管理者新規作成認証</title>
     <style>
         body {
     margin: 0;
--- U23ADMIN_DELEATE_CONFIRM.php
+++ U23ADMIN_DELEATE_CONFIRM.php
@@ -18,7 +18,7 @@ $id=trim($_POST['parent_account_ID'] ?? '');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ユーザー削除確認</title>
     <style>
         body {
     margin: 0;
